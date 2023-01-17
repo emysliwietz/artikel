c10dca631f0fd293eaece59b400dc354f418bd9c
Egidius <egidius@mysliwietz.de>
2023-01-17 16:22:15 +0100

    Language sensitive bible tooltips, wider 2 column geometry

--- btooltip.sh
+++ btooltip.sh
@@ -1,2 +1,11 @@
 #!  /usr/bin/env bash
-echo "\tooltip{$1 $2}{$(kjv -W $1 $2 | sed 1d)}{$1 $2}"
+CMD="kjv"
+case "$3" in
+    "german")
+        CMD="menge";;
+    "latin")
+        CMD="vul";;
+    "greek")
+        CMD="grb";;
+esac
+echo "\tooltip{$1 $2}{$($CMD -W $1 $2 | sed 1d)}{$1 $2}"
